92da3d8659ac19ea00547cf94bd415656723b2be
Calculator-0 <calculatorisverycool@gmail.com>
2026-07-15 08:53:46 +1200

    Changed up how menu works temporarily, added some assets.
    Added extra buttons to menu for ease of access between all points, added a large tree png which we can work our parkour / platforms around, this commit is mostly for menu but includes the tree

--- sticky-situation/scripts/menuHandler.gd
+++ sticky-situation/scripts/menuHandler.gd
@@ -5,3 +5,9 @@ func _on_play_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_level_testing_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/layoutTemp.tscn")
+
+func _on_player_development_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/playerDevelopment.tscn")
