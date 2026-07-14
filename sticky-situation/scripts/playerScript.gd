7600399849a1e9d81c2ab759fb481c4134f50e5b
EternalEagle <matthewsterk7@gmail.com>
2026-07-14 17:54:22 +1200

    another player update

--- sticky-situation/scripts/playerScript.gd
+++ sticky-situation/scripts/playerScript.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var walkAccel : int
 @export var walkSpeed : int
 @export var decceleration : int
-@export var jumpVelocity : int
+@export var jumpVelocity : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -41,7 +41,11 @@ func Walk() -> void:
 		velocity.x = clampf(velocity.x + GetInputDir() * walkAccel, -walkSpeed, walkSpeed)
 
 func Jump() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jumpVelocity
-		velocity.x += 0.5 * velocity.x # change to be for hold jump is mor boing boing
-		print(velocity.x)
+	if Input.is_action_pressed("jump") and is_on_floor():
+		print("The player is charging their jump")
+		# change the jump velocities
+	if Input.is_action_just_released("jump") and is_on_floor():
+		print("the player will jump now")
+		velocity.y = -jumpVelocity.y
+		velocity.x += jumpVelocity.x * velocity.x # change to be for hold jump is mor boing boing
+		print(velocity)
