fac3eebcffdfdef1e4af716ce14406303a3a9543
Calculator-0 <calculatorisverycool@gmail.com>
2026-07-14 21:10:57 +1200

    Temporary push whilst godot is slow

--- sticky-situation/scripts/menuHandler.gd
+++ sticky-situation/scripts/menuHandler.gd
@@ -1,9 +1,7 @@
 extends Node
 
-
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
--- sticky-situation/scripts/playerScript.gd
+++ sticky-situation/scripts/playerScript.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var walkSpeed : int
 @export var decceleration : int
 @export var jumpVelocity : Vector2
+var potentialVelocity : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -43,16 +44,20 @@ func Walk() -> void:
 	else:
 		velocity.x = clampf(velocity.x + GetInputDir() * walkAccel, -walkSpeed, walkSpeed)
 
-func Jump() -> void:
+#Changed the jump value to be lower so the charge has a bigger effect
+func Jump(delta) -> void:
 	if Input.is_action_pressed("jump") and is_on_floor():
 		print("The player is charging their jump")
-		# change the jump velocities
+		potentialVelocity += 10 * delta
+		print(potentialVelocity)
 	if Input.is_action_just_released("jump") and is_on_floor():
 		print("the player will jump now")
 		velocity.y = -jumpVelocity.y
 		velocity.x += jumpVelocity.x * velocity.x # change to be for hold jump is mor boing boing
 		print(velocity)
+		print(jumpVelocity.y)
 
+#Plays character animations relative to what they are doing
 func Animation() -> void:
 	if is_on_floor():
 		if GetInputDir() == 0:
@@ -62,6 +67,7 @@ func Animation() -> void:
 	else:
 		animatedSprite.play("Jump")
 
+#Flips the sprite depending on the direction they are going
 func AnimationDirection() -> void:
 	if GetInputDir() < 0 and velocity.x < 0:
 		animatedSprite.flip_h = true
