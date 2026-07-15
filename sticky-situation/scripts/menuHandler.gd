71f852c688f6177eef07695e501ee6a7dcd05b57
Annabelle <dixiea46@gmail.com>
2026-07-16 11:52:12 +1200

    tutorial made

--- sticky-situation/scripts/menuHandler.gd
+++ sticky-situation/scripts/menuHandler.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
