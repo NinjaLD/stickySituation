ced887220b9d708bdaa1377b3bb7bfa8c90aec2f
TylorHowie <tylorhowie@gmail.com>
2026-07-15 15:00:49 +1200

    did balance mini game stuff

--- sticky-situation/scripts/barMove.gd
+++ sticky-situation/scripts/barMove.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
+@export var speed : int
+@export var barDistance : int
 var dir : int
 
 func _process(delta: float) -> void:
 	Move(delta)
 
 func Move(deltaTime) -> void:
-	pass
+	position.x += speed * dir * deltaTime
