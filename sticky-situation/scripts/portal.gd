bcf5d4016488a2ddb883e4e448ef5a1ccb06bb32
Annabelle <dixiea46@gmail.com>
2026-07-16 12:24:26 +1200

    portal potral wefefeefusxkfruydbrfgudsgxcvbfgvebdv dxbhmsev dhsebjfdh
    :)

--- sticky-situation/scripts/portal.gd
+++ sticky-situation/scripts/portal.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if get_tree().current_scene.name == "tutorial":
+	if get_tree().current_scene.name == "Tutorial":
 		get_tree().call_deferred("change_scene_to_file", "res://scenes/game.tscn")
-	if get_tree().current_scene.name == "game":
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/game.tscn")
-		pass
+	if get_tree().current_scene.name == "Game":
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/tutorial.tscn")
